67ace6ea430b4395da936167e2d59295fc78ab63
Stephanie Tea <44644784+stephaniemtea@users.noreply.github.com>
2021-02-22 15:48:28 -0500

    Update addresses.sql

--- sql/addresses.sql
+++ sql/addresses.sql
@@ -1,5 +1,5 @@
 CREATE TABLE addresses(	
-	address_id INT PRIMARY KEY AUTO_INCREMENT,
+    address_id INT PRIMARY KEY AUTO_INCREMENT,
     address_one VARCHAR(255) NOT NULL,
     address_two VARCHAR(255),
     city VARCHAR(100) NOT NULL,
