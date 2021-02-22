531394b0bc1d20d10cff7723b392373eaaff79e6
Stephanie Tea <44644784+stephaniemtea@users.noreply.github.com>
2021-02-22 15:49:34 -0500

    Update and rename photoofthedays.sql to sql/photoofthedays.sql

--- sql/photoofthedays.sql
+++ sql/photoofthedays.sql
@@ -1,8 +1,8 @@
 CREATE TABLE photoOfTheDays(
 	photo_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    photo_name VARCHAR(100) NOT NULL,
-    photo_desc VARCHAR(255) NOT NULL,
-    photo_artistid VARCHAR(20) NOT NULL,
+    	photo_name VARCHAR(100) NOT NULL,
+    	photo_desc VARCHAR(255) NOT NULL,
+    	photo_artistid VARCHAR(20) NOT NULL,
 	photo_artistname VARCHAR(50) NOT NULL,
 	photo_post_date DATE NOT NULL,
 	photo_url VARCHAR(255) NOT NULL,
