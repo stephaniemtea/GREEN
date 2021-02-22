d0100f2c443c8dfd0060effdc114975d06effc7b
Stephanie Tea <44644784+stephaniemtea@users.noreply.github.com>
2021-02-22 15:51:09 -0500

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -1,13 +1,13 @@
 CREATE TABLE users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-	  first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    phone VARCHAR(10) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+	user_id INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(50) NOT NULL,
+    	last_name VARCHAR(50) NOT NULL,
+    	email VARCHAR(255) NOT NULL UNIQUE,
+    	phone VARCHAR(10) NOT NULL,
+    	password VARCHAR(255) NOT NULL,
    	username VARCHAR(255) NOT NULL UNIQUE,
-    user_address INT NOT NULL,
-	  user_current_points INT,
-	  user_level INT, 
-    FOREIGN KEY (user_address) REFERENCES addresses(address_id)
+    	user_address INT NOT NULL,
+	user_current_points INT,
+	user_level INT, 
+    	FOREIGN KEY (user_address) REFERENCES addresses(address_id)
 );
