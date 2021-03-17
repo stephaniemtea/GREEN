f6b97efb0a6ca4a5ca3c719b396d53bb6659815c
Stephanie Tea <st893@TERM-SVC6.cairn.edu>
2021-03-17 19:20:01 -0400

    adding php tests

--- index.php
+++ index.php
@@ -1,72 +1,52 @@
+<!-- Referenced: https://bbbootstrap.com/snippets/bootstrap-todo-list-custom-checkbox-button-ripple-effect-83884173 -->
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>GREEN</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>repl.it</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
 	<link href="https://fonts.googleapis.com/css2?family=Barlow&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="main.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
-	 crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-	 crossorigin="anonymous">
+	<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'>
+	<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js'>
 
 	</script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-	 crossorigin="anonymous">
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'>
 
 	</script>
+	<link href='https://use.fontawesome.com/releases/v5.7.2/css/all.css'>
 </head>
 
 <body>
+	<script src="script.js">
 
-	<nav class="navbar navbar-expand-sm navbar-dark bg-main-green">
-		<a class="navbar-brand " href="#">GREEN</a>
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" href="#">Events</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Articles</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Locator</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Rewards</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Social</a>
-			</li>
-
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">Account</a>
-				<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-					<li>
-						<a class="dropdown-item" href="#">Login</a>
-					</li>
-					<li>
-						<a class="dropdown-item" href="#">Register</a>
-					</li>
-				</ul>
-			</li>
-		</ul>
-	</nav>
-
+	</script>
 	<div class="container">
-		<h1 class="page-header" </h1>
-			<div class="row">
-				<div class="col-sm-8">
-					<h5 class="page-section my-3">Photo of the Day</h5>
-					<img class="img-fluid feature-image" src="https://images.unsplash.com/photo-1575986767340-5d17ae767ab0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1491&q=80" alt="Ninh Binh, Vietnam">    
-	        <h5 class="page-section my-3">Exclusive Online Bonus</h5>
-        </div>
-        <div class = "col-sm-4 p-3 my-3 border">
-          <h3 class="page-section">Tasks
-        </div>
+		<div class="col-md-6">
+			<h4 class="page-section">Tasks</h4>
+      <div class="task-section bg-white rounded info">Daily
+        <ul id="daily" class="tasks">
+          <?php
+            require "./includes/dbConnect.php";
+            $result = mysqli_query($conn, "SELECT task_name FROM `tasks` WHERE task_type = daily");
+            if (mysqli_num_rows($result) > 0) {
+              while ($row = mysqli_fetch_assoc($result)) {
+                echo '<li><label><input type="checkbox" class="taskItem option-input radio"><span class="label-text">'. $row['task_name']. '</span></label></li>';
+              }
+            } else {
+                echo "0 results";
+              }
+            require "./includes/dbDisconnect.php";
+          ?>
+        </ul>
+    </div>
+      <div class="task-section bg-white rounded info">Weekly
+        <ul id="weekly" class="tasks">
+          <li><label><input type="checkbox" class="taskItem option-input radio"><span class="label-text">Task 1</span></label></li>
+          <li><label><input type="checkbox" class="taskItem option-input radio"><span class="label-text">Task 2</span></label></li>
+        </ul>
       </div>
+    </div>
   </div>
-  
-</body>
 </html>
