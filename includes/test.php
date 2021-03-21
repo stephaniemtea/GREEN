a26523d5df00ef48e4e00d67dd0644180ea46531
Stephanie Tea <44644784+stephaniemtea@users.noreply.github.com>
2021-03-20 21:39:41 -0400

    Update test.php

--- includes/test.php
+++ includes/test.php
@@ -2,9 +2,8 @@
 
 require "dbConnect.php";
 
-// selects three random tasks 
-// $result = mysqli_query($conn, "SELECT task_id, task_name FROM tasks ORDER BY RAND() WHERE task_type='daily' LIMIT 3");
-$result = mysqli_query($conn, "SELECT task_id, task_name FROM tasks ORDER BY RAND()  LIMIT 3");
+// selects three random daily tasks 
+$result = mysqli_query($conn, "SELECT task_id, task_name FROM tasks WHERE task_type='daily' ORDER BY RAND() LIMIT 3");
 
 if (mysqli_num_rows($result) > 0) {
   while($row = mysqli_fetch_assoc($result)) {
