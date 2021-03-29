921ccccabccbd40a8e7feb2d0f6aa820b87c3c4e
Stephanie Tea <44644784+stephaniemtea@users.noreply.github.com>
2021-03-29 14:43:54 -0400

    Update icebreakers.sql

--- sql/icebreakers.sql
+++ sql/icebreakers.sql
@@ -4,7 +4,7 @@ CREATE TABLE icebreakers (
     icebreaker_type VARCHAR(10) NOT NULL
 );
 
-INSERT INTO icebreakers (icebreaker_name, icebreaker_type) VALUES ('How long was your shower yesterday or this morning? Try to keep track of how much time you spend in the shower and work on reducing that time.', 'question');
+INSERT INTO icebreakers (icebreaker_name, icebreaker_type) VALUES ('How long was your shower yesterday or this morning?', 'question');
 INSERT INTO icebreakers (icebreaker_name, icebreaker_type) VALUES ('Go on a 10-15 minute walk outside today and pick up any trash you see along the way.', 'challenge');
 INSERT INTO icebreakers (icebreaker_name, icebreaker_type) VALUES ('Would you rather be the smartest person in the room or the funniest?', 'question');
 INSERT INTO icebreakers (icebreaker_name, icebreaker_type) VALUES ('If you couldn''t fail at whatever you tried, what would you try first and why?', 'question');
